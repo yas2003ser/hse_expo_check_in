02e6bf83b4b8487295964fa0ea099578f8996b9c
yasser <yasser@yasser.yasser>
2024-05-29 00:34:11 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/hse_expo_check/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hse_expo_check
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:hse_expo_check/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
